9eed3d37d59a060f6b8b3d51d00ad0a05ab57657
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2024-01-16 16:12:48 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33076 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,13 @@
+word = input().lower()
+word_list = list(set(word))
+cnt_list = []
+
+for i in word_list:
+    cnt = word.count(i)
+    cnt_list.append(cnt)
+    
+if cnt_list.count(max(cnt_list)) > 1:
+    print("?")
+
+else:
+    print(word_list[(cnt_list.index(max(cnt_list)))].upper())
