d752bfe68911d9871045c26048ae23466dd87c6b
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2024-01-18 19:46:46 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,13 @@
+N = int(input())
+nums = map(int, input().split())
+cnt = 0
+
+for i in nums:
+    for j in range(2, i+1):
+        if i % j == 0:
+            if i == j:
+                cnt +=1
+                
+            break
+     
+print(cnt)
