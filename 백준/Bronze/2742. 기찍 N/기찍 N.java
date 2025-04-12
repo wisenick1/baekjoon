3ddbf783d151755f6183d24c1209c067b28d3be4
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2025-04-12 22:02:57 +0900

    [Bronze IV] Title: 기찍 N, Time: 840 ms, Memory: 32416 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int i = sc.nextInt();
+
+        for (int j = i; j > 0; j--) {
+            System.out.println(j);
+        }
+
+    }
+}
