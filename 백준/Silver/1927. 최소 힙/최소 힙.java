d8d5b3370d478a4910f3dca9a2886d5b3e4fc064
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2025-04-13 20:29:43 +0900

    [Silver II] Title: 최소 힙, Time: 788 ms, Memory: 33420 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main (String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            long x = Long.parseLong(br.readLine());
+            if (x > 0) {
+                queue.add(x);
+            } else {
+                if (queue.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(queue.poll());
+                }
+            }
+
+        }
+    }
+}
