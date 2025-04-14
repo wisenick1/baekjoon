7bc4a53588a9c73d11e70ca8d209f6429964f332
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2025-04-14 20:39:54 +0900

    [Bronze II] Title: 수 정렬하기, Time: 292 ms, Memory: 22384 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,22 +1,30 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) {
+public class Main{
+    public static void main (String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-        List<Integer> numbers = new ArrayList<>();
 
-        for (int i = 0; i < input; i++) {
-            int input2 = sc.nextInt();
-            numbers.add(input2);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        Collections.sort(numbers);
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
 
-        for (Integer num : numbers) {
-            System.out.println(num);
+        for (int i : arr) {
+            System.out.println(i);
         }
     }
 }
