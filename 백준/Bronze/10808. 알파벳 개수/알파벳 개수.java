b890c7c1a00135e15ef52f1625d02489e6d61e54
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2025-04-20 14:45:47 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 188 ms, Memory: 18348 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -6,24 +6,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
 
-        String[] arr = new String[26];
         int[] result = new int[26];
 
-        for (int i = 0; i < 26; i++) {
-            arr[i] = String.valueOf((char)(97+i));
+        for (int i = 0; i < s.length(); i++) {
+            result[s.charAt(i) - 97]++;
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < 26; j++) {
-                if (s.substring(i, i + 1).equals(arr[j])) {
-                    result[j] += 1;
-                }
-            }
-        }
         for (int i : result) {
             System.out.print(i + " ");
         }
-
     }
-
 }
