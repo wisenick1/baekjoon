7ea9a83f55a3d20e366ba05860c7f8a61778959c
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2025-05-09 14:35:55 +0900

    [Gold IV] Title: N-Queen, Time: 2756 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static boolean[] isUsed1 = new boolean[15];
+    static boolean[] isUsed2 = new boolean[30];
+    static boolean[] isUsed3 = new boolean[30];
+    static int[][] arr = new int[15][15];
+    static int cnt = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        nQueen(0);
+        System.out.println(cnt);
+
+    }
+
+    private static void nQueen(int cur) {
+        if (cur == n) {
+            cnt++;
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!isUsed1[i] && !isUsed2[cur + i] && !isUsed3[cur - i + n - 1]) {
+                isUsed1[i] = true;
+                isUsed2[cur + i] = true;
+                isUsed3[cur - i + n - 1] = true;
+                nQueen(cur + 1);
+                isUsed1[i] = false;
+                isUsed2[cur + i] = false;
+                isUsed3[cur - i + n - 1] = false;
+            }
+        }
+    }
+}
