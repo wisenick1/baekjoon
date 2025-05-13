216fbdcf5fd1756215a41ea14e46ad26a1bdc0b3
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2025-05-13 14:41:03 +0900

    [Silver III] Title: 1로 만들기, Time: 200 ms, Memory: 21708 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] d = new int[1000001];
+        d[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            d[i] = d[i - 1] + 1;
+            if (i % 3 == 0) {
+                d[i] = Math.min(d[i / 3] + 1, d[i]);
+            }
+            if (i % 2 == 0) {
+                d[i] = Math.min(d[i / 2] + 1, d[i]);
+            }
+        }
+        System.out.println(d[n]);
+    }
+}
